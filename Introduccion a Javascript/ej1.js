364ba096a8bcbcfc8ce40865c360eee854150d41
MEscriba <martinescribabab@gmail.com>
2023-01-14 19:48:54 -0300

    se hizo ej1 y se probo con node

--- Introduccion a Javascript/ej1.js
+++ Introduccion a Javascript/ej1.js
@@ -4,3 +4,7 @@ let edad= 38;
 let desarrollador=true;
 let fechanac = new Date (1984, 5, 26);
 console.log(fechanac);
+
+let ejercicio = new Array (nombre, edad, desarrollador, fechanac);
+
+console.log(ejercicio);
