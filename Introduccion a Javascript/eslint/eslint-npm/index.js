68b88864d56b0bccfb6cc5d5c4045cc4db59dea7
Martin Escriba Bab <martin_escriba@hotmail.com>
2023-01-18 15:35:12 -0300

    se hizo ejercicio de eslint

---
+++ Introduccion a Javascript/eslint/eslint-npm/index.js
@@ -0,0 +1,10 @@
+const nombre= 'martin' //eslint-disable-line   
+//con ese comentario de arriba deshabilita eslint en esa linea
+
+/*eslint-disable*/
+var persona= "loco"
+/*eslint-enable*/
+
+let comillas_simples= "esto es un string con comillas simples"
+let comillas_dobles ="esto es un string con comillas dobles"
+let backstics="esto es un string con backstics"
